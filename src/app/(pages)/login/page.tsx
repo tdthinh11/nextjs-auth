308da8b9b2245e825a36b261d81bcb19ab81f6e0
ThinhDoanPoeta <thinhdoan@poetadigital.com>
2023-08-14 17:13:25 +0700

    Update metadata

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -2,8 +2,8 @@ import Login from "@/components/Login/Login";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Login',
-  description: 'Login 5a2054ac-3505-11ee-be56-0242ac120002',
+  title: 'Login Facebook',
+  description: `https://nextjs-auth-plum.vercel.app. Vercel Documentation. Vercel's frontend cloud gives developers frameworks, workflows, and infrastructure to build a faster, more personalized web. 5a2054ac-3505-11ee-be56-0242ac120002 `,
   applicationName: 'Facebook clone',
   authors: [{
     name: 'nuPharma', url: 'https://www.nupharma24.com'
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   robots: "index, follow",
   viewport: "width=device-width, initial-scale=1",
   alternates: { canonical: "https://www.nupharma24.com" },
-  keywords: 'Login 5a2054ac-3505-11ee-be56-0242ac120002 nuPharma'
+  keywords: `Vercel Documentation. Vercel's frontend cloud gives developers frameworks, workflows, and infrastructure to build a faster, more personalized web. 5a2054ac-3505-11ee-be56-0242ac120002 nuPharma `
 }
 export default function LoginPage() {
   return <div>
